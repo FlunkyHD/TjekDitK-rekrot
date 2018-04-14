34b24642a9e945935a9dba57f2aa3ba8a5cbd5cc
Morten jørgensen <mortenj2@hotmail.dk>
2018-04-14 11:36:55 +0200

    nice

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-/* Registration process, inserts user info into the database 
+/* Registration process, inserts user info into the database
    and sends account confirmation email message
  */
 
@@ -14,21 +14,21 @@ $last_name = $mysqli->escape_string($_POST['lastname']);
 $email = $mysqli->escape_string($_POST['email']);
 $password = $mysqli->escape_string(password_hash($_POST['password'], PASSWORD_BCRYPT));
 $hash = $mysqli->escape_string( md5( rand(0,1000) ) );
-      
+
 // Check if user with that email already exists
 $result = $mysqli->query("SELECT * FROM users WHERE email='$email'") or die($mysqli->error());
 
 // We know user email exists if the rows returned are more than 0
 if ( $result->num_rows > 0 ) {
-    
-    $_SESSION['message'] = 'User with this email already exists!';
+
+    $_SESSION['message'] = 'Bruger med denne E-mail findes allerede';
     header("location: error.php");
-    
+
 }
 else { // Email doesn't already exist in a database, proceed...
 
     // active is 0 by DEFAULT (no need to include it here)
-    $sql = "INSERT INTO users (first_name, last_name, email, password, hash) " 
+    $sql = "INSERT INTO users (first_name, last_name, email, password, hash) "
             . "VALUES ('$first_name','$last_name','$email','$password', '$hash')";
 
     // Add user to the database
@@ -37,25 +37,31 @@ else { // Email doesn't already exist in a database, proceed...
         $_SESSION['active'] = 0; //0 until user activates their account with verify.php
         $_SESSION['logged_in'] = true; // So we know the user has logged in
         $_SESSION['message'] =
-                
+
                  "Confirmation link has been sent to $email, please verify
                  your account by clicking on the link in the message!";
 
         // Send registration confirmation link (verify.php)
+
         $to      = $email;
-        $subject = 'Account Verification ( clevertechie.com )';
-        $message_body = '
+        $subject = 'Aktivering af Bruger ( Digitalt Kørekort )';
+        $message = '
         Hello '.$first_name.',
 
         Tak for at tilmelde dig vores hjemmeside!
 
         Klik på nedenstående link for at aktivere din bruger:
 
-        http://localhost/login/verify.php?email='.$email.'&hash='.$hash;  
+        http://localhost/login/verify.php?email='.$email.'&hash='.$hash;
 
-        mail( $to, $subject, $message_body );
+        $message = wordwrap($message, 70);
 
-        header("location: profile.php"); 
+        mail( 'mortenj2@hotmail.dk', $subject, $message );
+
+
+        mail("morten@joorgensen.dk","Success","Send mail from localhost using PHP");
+
+        header("location: profile.php");
 
     }
 
