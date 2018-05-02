ce71d67709a8bbcb34ca202a7c18edc7d0a02b45
Morten jørgensen <mortenj2@hotmail.dk>
2018-05-02 10:56:51 +0200

    xD

--- db.php
+++ db.php
@@ -2,6 +2,6 @@
 /* Database connection settings */
 $host = 'mysql20.123hotel.dk';
 $user = 'kajkager';
-$pass = 'MortenogPeter!';
+$pass = 'jiA9!jSDAa';
 $db = 'kajkager_dk_db';
 $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
